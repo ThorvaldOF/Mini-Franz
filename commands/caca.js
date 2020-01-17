ce0b592b744dbd25459c7b638cac8b5545beed70
Franz <55585297+ThorvaldOF@users.noreply.github.com>
2020-01-17 15:34:41 +0100

    Add files via upload

---
+++ commands/caca.js
@@ -0,0 +1,33 @@
+module.exports = class test {
+    constructor(){
+        this.name = 'caca',
+        this.alias = ['t'],
+        this.usage = '.caca'
+    }
+
+    run(client, message, args){
+        
+        if (args[1] = 114){
+            
+        client.channels.get("667737105778343956").send("114")
+        }
+        if (args[1] = 115){
+            
+        client.channels.get("667737105778343956").send("115")
+        }
+        if (args[1] = 116){
+            
+        client.channels.get("667737105778343956").send("116")
+        }
+        if (args[1] = 117){
+            
+        client.channels.get("667737105778343956").send("117")
+        }
+        if (args[1] = 118){
+            
+        client.channels.get("667737105778343956").send("118")
+        }
+        
+        
+    }
+}
